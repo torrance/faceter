7d46c0c09289de804c8d2a074970d61f0d4c083f
Torrance Hodgson <torrance123@gmail.com>
2019-04-23 12:35:10 +0800

    Add timestep handling to applysolution.py

--- applysolution.py
+++ applysolution.py
@@ -19,7 +19,6 @@ def main(args):
     dest = args.dest.upper()
     nrows = args.nrows
 
-
     if src not in mset.colnames():
         print("%s is not a valid column name" % src, file=sys.stderr)
         exit(1)
@@ -37,14 +36,21 @@ def main(args):
     if args.reverse:
         solution = linalg.inv(solution)
 
+    # Handle channels
     nchans, _ = mset.getcell(src, 0).shape
     width = nchans // solution.shape[2]
-    idxs = np.array(range(0, nchans), dtype=int) // width
-    lhs = solution[:, :, idxs]
-    rhs = np.transpose(np.conj(solution), [0, 1, 2, 4, 3])[:, :, idxs]
+    chan_idxs = np.array(range(0, nchans), dtype=int) // width
+    lhs = solution[:, :, chan_idxs]
+    rhs = np.transpose(np.conj(solution), [0, 1, 2, 4, 3])[:, :, chan_idxs]
 
-    assert(solution.shape[0] == 1)  # Timesteps not handled yet
-    assert(nchans % solution.shape[2] == 0)  # Width must be integer
+    # Handle timesteps
+    # This logic is replicated from calibrate (it's certainly not how I'd do it!)
+    ntimes = solution.shape[0]
+    unique_times = np.array(sorted(set(mset.getcol('TIME'))))
+    time_breaks = (np.array(range(0, ntimes + 1)) * len(unique_times)) // ntimes
+    unique_time_idxs = np.empty_like(unique_times, dtype=int)
+    for i, (lower, upper) in enumerate(zip(time_breaks[:-1], time_breaks[1:])):
+        unique_time_idxs[lower:upper] = i
 
     for n in range(0, len(mset) // nrows + 1):
         offset = n * nrows
@@ -55,13 +61,14 @@ def main(args):
         sys.stderr.flush()
         ants1 = submset.getcol('ANTENNA1')
         ants2 = submset.getcol('ANTENNA2')
+        time_idxs = unique_time_idxs[np.where(submset.getcol('TIME')[:, None] == unique_times)[1]]
         data = submset.getcol(src)
         print("Done", file=sys.stderr)
 
         print("- Applying corrections... ", end="", file=sys.stderr)
         sys.stderr.flush()
         data = np.reshape(data, (data.shape[0], data.shape[1], 2, 2))
-        corrected = np.matmul(lhs[0, ants1], np.matmul(data, rhs[0, ants2]))
+        corrected = np.matmul(lhs[time_idxs, ants1], np.matmul(data, rhs[time_idxs, ants2]))
         corrected = np.reshape(corrected, (data.shape[0], data.shape[1], 4))
         print("Done", file=sys.stderr)
 
